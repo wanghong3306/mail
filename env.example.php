7f3e65cce9f833c9ba84a659fa89656f08927312
wanghong <844659503@qq.com>
2020-05-03 11:46:37 +0800

    xx

---
+++ env.example.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'accountArr' => [
+        '163' => [
+            'user' => 'xx@163.com',
+            'pass' => 'xx',
+            'host' => 'smtp.163.com',
+        ],
+        'qq' => [
+            'user' => 'xx@qq.com',
+            'pass' => 'xx',
+            'host' => 'smtp.qq.com',
+        ],
+    ],
+];
